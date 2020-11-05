27db38825c55d759acbf23bcbdeac8ab628be561
westee <904885508@qq.com>
2020-11-05 22:44:45 +0800

    表结构设计与JPA初步使用 part one

---
+++ src/main/java/com/github/westee/course/model/User.java
@@ -0,0 +1,83 @@
+package com.github.westee.course.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Entity
+@Table(name = "user")
+public class User {
+    private Integer id;
+    private String username;
+    private String encrypted_password;
+    private String email;
+    private Date created_at;
+    private Date updated_at;
+    private String status;
+
+    @Id
+    @Column(name = "id")
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Column(name = "username")
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Column(name = "encrypted_password")
+    public String getEncrypted_password() {
+        return encrypted_password;
+    }
+
+    public void setEncrypted_password(String encrypted_password) {
+        this.encrypted_password = encrypted_password;
+    }
+
+    @Column(name = "email")
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Column(name = "created_at")
+    public Date getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Date created_at) {
+        this.created_at = created_at;
+    }
+
+    @Column(name = "updated_at")
+    public Date getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(Date updated_at) {
+        this.updated_at = updated_at;
+    }
+
+    @Column(name = "status")
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
---
+++ src/main/resources/db/migration/V1__CreateUserTable.sql
@@ -0,0 +1,53 @@
+create table "user" (
+    id serial primary key,
+    username varchar(20) unique not null,
+    encrypted_password varchar(20) not null,
+    email varchar(255)  not null,
+    created_at timestamp not null default now(),
+    updated_at timestamp not null default now(),
+    status varchar(10) not null default 'OK'
+);
+
+create table role (
+    id serial primary key,
+    name varchar(20) unique not null,
+    created_at timestamp not null default now(),
+    updated_at timestamp not null default now(),
+    status varchar(10) not null default 'OK'
+);
+
+insert into role (id, name) values(1, '学生');
+insert into role (id, name) values(2, '老师');
+insert into role (id, name) values(3, '管理员');
+
+create table user_role (
+    id serial primary key,
+    user_id integer not null,
+    role_id integer not null,
+    created_at timestamp not null default now(),
+    updated_at timestamp not null default now(),
+    status varchar(10) not null default 'OK'
+);
+
+insert into "user" (id, username, encrypted_password, email) values (1, '学生', '123456', '123@qq.com');
+insert into "user" (id, username, encrypted_password, email) values (2, '老师', '123456', '123@qq.com');
+insert into "user" (id, username, encrypted_password, email) values (3, '管理员', '123456', '123@qq.com');
+insert into user_role (id, user_id, role_id) values (1, 1, 1);
+insert into user_role (id, user_id, role_id) values (2, 2, 2);
+insert into user_role (id, user_id, role_id) values (3, 3, 3);
+
+create table permission (
+    id serial primary key,
+    name varchar(50) not null,
+    role_id integer not null,
+    created_at timestamp not null default now(),
+    updated_at timestamp not null default now(),
+    status varchar(10) not null default 'OK'
+);
+
+insert into permission ( name, role_id) values ('登录用户', 1);
+insert into permission ( name, role_id) values ('登录用户', 2);
+insert into permission ( name, role_id) values ('登录用户', 3);
+insert into permission ( name, role_id) values ('上传课程', 2);
+insert into permission ( name, role_id) values ('上传课程', 3);
+insert into permission ( name, role_id) values ('管理用户', 3);
